9eac1d687e5022292fbc275bc50f3b9982206246
kweku-45 <kebiney@hotmail.com>
2021-08-20 15:41:43 +0000

    Printer error

---
+++ printer_error.py
@@ -0,0 +1,17 @@
+def printer_error(s):
+    length = len(s)
+    count = 0
+    valid = "abcdefghijklm"
+    for x in s:
+        if x not in valid:
+            count+=1    
+    return f"{count}/{length}"
+
+print(printer_error("adjhjdcbdjjdjs"))
+
+"""
+New thing learnt:
+
+You can use > when conparing letters
+
+"""
